dfc58a3c045834435b5c4f4a5b9227c2a819bd2c
1ninjwarrior <jadendawdy@gmail.com>
2025-02-20 13:58:00 -0700

    remove import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { DeepSeekAPI } from 'deepseek-api';
 
 function App() {
   const [userMessage, setUserMessage] = useState('');
