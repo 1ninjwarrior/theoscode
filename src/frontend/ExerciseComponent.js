aa16437a9a40168075828da354614a6cb4c2e1bc
1ninjwarrior <jadendawdy@gmail.com>
2025-02-24 17:26:25 -0700

    add inputs

--- src/frontend/ExerciseComponent.js
+++ src/frontend/ExerciseComponent.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { FaTrash } from 'react-icons/fa';
+
+class Sets {
+  reps = 0;
+  weight = 0;
+}
 
 const ExerciseComponent = ({ index, response }) => {
-  // Assuming response is an object, access the correct property
 
+  // Assuming response is an object, access the correct property
+  const [sets, setSets] = useState([Sets]);
   return (
     <div>
       <p>{response.content}</p>
+      <button onClick={() => setSets([...sets, new Sets()])}>+</button>
+      <button onClick={() => setSets(sets.slice(0, -1))}><FaTrash /></button>
+      <div>
+        {sets.map((set, index) => (
+          <div key={index}>
+          <input type="number" initialValue={set.reps} onChange={(e) => set.reps = e.target.value} />
+          <input type="number" initialValue={set.weight} onChange={(e) => set.weight = e.target.value} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
